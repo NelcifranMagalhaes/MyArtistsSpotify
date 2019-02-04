b265835e51c539a79e69fcdccccb9ce7716c9848
nelcifran <nelcifranpires@gmail.com>
2019-02-04 10:05:59 -0300

    config in application.rb changed

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module SearchSpot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.middleware.insert_after ActiveRecord::Migration::CheckPending, ActionDispatch::Cookies
-    config.middleware.insert_after ActionDispatch::Cookies, ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
